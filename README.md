55bfccdbd2b54e30fbce34e6e6391cf5a2ea7b25
zackWitherspoon <frisbex@gmail.com>
2023-01-16 16:38:25 -0500

    Updated read me

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # mag-mutual-tech-screening
 mag-mutual-tech-screening
+
+This is a project for the tech screening of Mag Mutual.
+
+### Description:
+At MagMutual we are always building new microservices that replace older monolithic systems.
+This project is a smaller feature set of what we do on a weekly/monthly basis.
+We would like you to build a Microservice that exposes a couple endpoints to return the
+following information. We would also like a simple UI to display the information.
+We have provided a CSV of user information; we would like you to build to build the following
+endpoints:
+- Endpoint to return a specific user (and all associated information)
+- Endpoint to return a list of users created between a date range
+- Endpoint to return a list of users based on a specific profession
+- Custom Endpoint that you design on your own.
+  Assumptions:
+- You can build it in any language/framework combination that you like, or feel is best.
+
+Assumptions:
+- You can build it in any language/framework combination that you like, or feel is best.
+
+Deliverables:
+- The artifacts or link to the source code.
+- Answer the following questions:
+  - What did you think of the project?
+    - The project has a lot of room for creation and self-expression in the code. It's a simple enough project that also demands good architectural design and understanding of a chosen language.
+  - What didn’t you like about the project?
+    - Nothing. It lets you get as in depth as you want, or you can be as broad as you want. 
+  - How would you change the project or approach?
+    - For individual roles you're targeting you might want to add conditional criteria to the prompt. I.e. if you're going for a devops role, add that they need to use github interactions or an architecture role would need to have a design doc. 
+  - Anything else you would like to share?
+    - Nope. Fun tech screening, and I'm excited to talk about my code :)
+
+### Execution steps:
+```
+go build
+go run .
+```
--- user/service.go
+++ user/service.go
@@ -14,6 +14,12 @@ import (
 	"time"
 )
 
+const (
+	idErrorMessage                   = "Id used in an incorrect format. Try again with a different id"
+	dateUnParsableErrorMessage       = "fromDate or toDate unable to be parsed. Please try again with a different date"
+	professionUnParsableErrorMessage = "Unable to unmarshal request. Try with a different profession"
+)
+
 type V1Service struct {
 	Users *[]model.User
 }
@@ -34,7 +40,13 @@ func (service *V1Service) GetAllUsers(w http.ResponseWriter, request *http.Reque
 func (service *V1Service) GetUserById(writer http.ResponseWriter, request *http.Request) {
 	log.Info("Endpoint Hit: etUsersById")
 	requestVariables := mux.Vars(request)
-	id, _ := strconv.Atoi(requestVariables["id"])
+	id, err := strconv.Atoi(requestVariables["id"])
+	if err != nil {
+		error := fmt.Sprint(idErrorMessage)
+		log.Error(error, err)
+		http.Error(writer, error, http.StatusBadRequest)
+		return
+	}
 
 	var foundUsers []model.User
 	for _, user := range *service.Users {
@@ -56,8 +68,13 @@ func (service *V1Service) GetUsersByDatePage(writer http.ResponseWriter, request
 	log.Info("Endpoint Hit: getUsersByDate")
 
 	query := request.URL.Query()
-	fromDate, _ := time.Parse(constants.DateFormat, query.Get("fromDate"))
-	toDate, _ := time.Parse(constants.DateFormat, query.Get("toDate"))
+	fromDate, err := time.Parse(constants.DateFormat, query.Get("fromDate"))
+	toDate, err := time.Parse(constants.DateFormat, query.Get("toDate"))
+	if err != nil {
+		log.Error(dateUnParsableErrorMessage, err)
+		http.Error(writer, dateUnParsableErrorMessage, http.StatusBadRequest)
+		return
+	}
 	var foundUsers []model.User
 	for _, user := range *service.Users {
 		if user.DateCreated.After(fromDate) && user.DateCreated.Before(toDate) || user.DateCreated.Equal(fromDate) ||
@@ -81,7 +98,9 @@ func (service *V1Service) GetUsersByProfessionPage(writer http.ResponseWriter, r
 	log.Infof(string(body))
 	var reqProfession model.APIProfessionRequest
 	if err := json.Unmarshal(body, &reqProfession); err != nil {
-		log.Error("Unable to unmarshal request. Tried ", string(body), " but was unable to unmarshal.")
+		log.Error(professionUnParsableErrorMessage, err)
+		http.Error(writer, professionUnParsableErrorMessage, http.StatusBadRequest)
+		return
 	}
 
 	var foundUsers []model.User
