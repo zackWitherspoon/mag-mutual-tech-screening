baf146d5989a6594dbd68d3bc5aedf6cb2ca37f3
zackWitherspoon <frisbex@gmail.com>
2023-01-22 16:18:43 -0500

    Fixed a dispaly problem when building the project

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	log "github.com/sirupsen/logrus"
+	"mag_matual_tech_project/config"
+)
 
 func main() {
 	log.Info("Application is starting up")
-	routes := NewRoutes(Configuration{}.NewConfiguration())
+	routes := config.NewRoutes(config.Configuration{}.NewConfiguration())
 	log.Info("Routes have been configured")
 	routes.LoadRoutes()
 }
